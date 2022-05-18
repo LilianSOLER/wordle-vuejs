356359983f9978a62d58568234d35665ce64a801
SOLER Lilian <soler.lilian64@gmail.com>
2022-05-18 18:50:17 +0200

    Client keyboard

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import { onMounted } from "vue";
 
 const handleInput = (button) => {
-  console.log(button);
+	console.log(button);
 };
-</script>
 
+onMounted(() => {
+	window.addEventListener("keyup", (e) => {
+		e.preventDefault();
+		let key =
+			e.key == "enter"
+				? "{enter}"
+				: e.key == "backspace"
+				? "{bksp}"
+				: e.key.toLowerCase();
+		handleInput(key);
+	});
+});
+</script>
 
 <template>
 	<div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
