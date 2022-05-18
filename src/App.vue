3df3b8e88e8036138f63c6cef486bb0977080572
SOLER Lilian <soler.lilian64@gmail.com>
2022-05-18 19:19:33 +0200

    Handling input from both keyboard

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,25 @@ import WordRow from "./components/WordRow.vue";
 import { reactive, onMounted } from "vue";
 
 const state = reactive({
-  solution: "books",
-  guesses: ["", "", "", "", ""],
-  currentGuessIndex: 0,
+	solution: "books",
+	guesses: ["", "", "", "", ""],
+	currentGuessIndex: 0,
 });
 
-const handleInput = (button) => {
-	console.log(button);
+const handleInput = (key) => {
+	const currentGuess = state.guesses[state.currentGuessIndex];
+
+	if (key == "{enter}") {
+		// Submit the current guess
+	} else if (state.currentGuessIndex >= 6) {
+		return;
+	} else if (key == "{bksp}") {
+		state.guesses[state.currentGuessIndex] = currentGuess.slice(0, -1); // remove last character
+	} else if (currentGuess.length < 5) {
+		if (/[a-zA-Z]/.test(key)) {
+			state.guesses[state.currentGuessIndex] += key;
+		}
+	}
 };
 
 onMounted(() => {
@@ -29,18 +41,17 @@ onMounted(() => {
 
 <template>
 	<div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
-    <div>
-      <WordRow
-        v-for="(guesses, i) in state.guesses"
-        :key="i"
-        :value="guesses"
-        :solutions="state.solution"
-        :submitted=" i < state.currentGuessIndex"
-      />
-    </div>
+		<div>
+			<WordRow
+				v-for="(guesses, i) in state.guesses"
+				:key="i"
+				:value="guesses"
+				:solutions="state.solution"
+				:submitted="i < state.currentGuessIndex"
+			/>
+		</div>
 		<SimpleKeyboard @onKeyPress="handleInput" />
 	</div>
 </template>
 
-<style>
-</style>
+<style></style>
